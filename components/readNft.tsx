22ab69f3817dabe6d4c3dd2819e2926dec68ae7d
pieakshat <145490318+pieakshat@users.noreply.github.com>
2024-09-05 06:14:24 +0530

    added readNft.tsx

--- components/readNft.tsx
+++ components/readNft.tsx
@@ -1,42 +1,27 @@
-import { ethers } from "ethers";
-import ERC721_ABI from "../abis/erc721ABI.json";
+// Setup: npm install alchemy-sdk
+import { Alchemy, Network } from "alchemy-sdk";
 
-async function fetchNFTsByAddress(
-    provider: ethers.Provider,
-    contractAddress: string,
-    userAddress: string
-) {
-    const contract = new ethers.Contract(contractAddress, ERC721_ABI, provider);
+const config = {
+    apiKey: "ObRBmbXEtBVr6aVMb4XE4yPd7FajQh63",
+    network: Network.ETH_MAINNET,
+};
+const alchemy = new Alchemy(config);
 
-    // Get the number of NFTs the user owns
-    const balance = await contract.balanceOf(userAddress);
-    console.log(`Address ${userAddress} owns ${balance.toString()} NFTs`);
+const main = async () => {
+    // Get all NFTs
+    const nfts = await alchemy.nft.getNftsForOwner("0xAB38Faa6aba27921E54Cf2E740B9BdE0d3Ee3994");
+    // Print NFTs
+    console.log(nfts);
+};
 
-    const nfts = [];
-
-    for (let i = 0; i < balance; i++) {
-        const tokenId = await contract.tokenOfOwnerByIndex(userAddress, i);
-        const tokenURI = await contract.tokenURI(tokenId);
-        nfts.push({ tokenId: tokenId.toNumber(), tokenURI });
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
     }
+};
 
-    return nfts;
-}
-
-async function main() {
-    // Initialize provider (e.g., using Infura, Alchemy, or any other provider)
-    const provider = new ethers.providers.JsonRpcProvider("https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID");
-
-    // Replace with your NFT contract address and user address
-    const contractAddress = "0xYourNFTContractAddress"; // Replace with the NFT contract address
-    const userAddress = "0xUserAddress"; // Replace with the user's Ethereum address
-
-    const nfts = await fetchNFTsByAddress(provider, contractAddress, userAddress);
-
-    console.log("NFTs held by the user:");
-    nfts.forEach((nft, index) => {
-        console.log(`NFT ${index + 1}: Token ID = ${nft.tokenId}, Token URI = ${nft.tokenURI}`);
-    });
-}
-
-main().catch(console.error);
+runMain();
